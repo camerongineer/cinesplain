1b0141320292016293a7a9f82b645b4a5d371ef0
camerongineer <alphaomegabeta@gmail.com>
2024-06-08 07:04:46 -0700

    feat: add move page loader file

---
+++ cinesplain.client/src/loaders/moviePageLoader.ts
@@ -0,0 +1,55 @@
+import { FetchQueryOptions, QueryClient } from "@tanstack/react-query";
+import { Params } from "react-router-dom";
+import {
+    getRecommendedMoviesPath,
+    retrieveCredits,
+    retrieveMovie,
+    retrieveMovies,
+    retrieveOmdbMovieDetails
+} from "../api/moviesApi";
+import Credits from "../types/credits";
+import Movie from "../types/movie";
+import { getNumericId } from "../utils/formatUtils";
+
+interface LoaderData {
+    movie: Movie;
+    credits: Credits | null;
+}
+
+const moviePageQuery = (movieId: string | undefined): FetchQueryOptions<LoaderData> => ({
+    queryKey: ["moviePage", movieId],
+    queryFn: async (): Promise<LoaderData> => {
+        const movie = await retrieveMovie(getNumericId(movieId ?? ""));
+        if (!movie) {
+            throw new Error("This page doesn't not exist.");
+        }
+        const credits = await retrieveCredits(movie.id);
+
+        return { movie, credits };
+    }
+});
+
+const movieRecommendationsQuery = (movieId: number) => ({
+    queryKey: ["movieRecommendations", movieId],
+    queryFn: async () => {
+        let recommendations = await retrieveMovies(getRecommendedMoviesPath(movieId));
+        return recommendations?.filter((movie: { backdropPath: string }) => movie.backdropPath) ?? null;
+    }
+});
+
+const omdbQuery = (imdbId: string) => ({
+    queryKey: ["moviePageOmdb", imdbId],
+    queryFn: async () => retrieveOmdbMovieDetails(imdbId)
+});
+
+const moviePageLoader =
+    (queryClient: QueryClient) =>
+    async ({ params }: { params: Params }) => {
+        const movieId = params.movieId;
+        const query = moviePageQuery(movieId);
+        const data: LoaderData | undefined = queryClient.getQueryData(query.queryKey);
+        return data ?? (await queryClient.fetchQuery(moviePageQuery(movieId)));
+    };
+
+export { moviePageLoader, moviePageQuery, movieRecommendationsQuery, omdbQuery };
+
