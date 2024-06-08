da571fd3752acd1ac086db4f44ecd5fab57a3710
camerongineer <alphaomegabeta@gmail.com>
2024-06-08 07:06:44 -0700

    refactor: remove loader code

--- cinesplain.client/src/ui/components/content/homePage/HomePage.tsx
+++ cinesplain.client/src/ui/components/content/homePage/HomePage.tsx
@@ -1,49 +1,18 @@
 import { Grid, Stack } from "@mui/material";
 import { blue, pink, purple, red } from "@mui/material/colors";
-import { QueryClient, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
-import {
-    getClassicMoviesPath,
-    getMostHatedMoviesPath,
-    getMostLovedMoviesPath,
-    getNowPlayingMoviesPath,
-    getUpcomingMoviesPath,
-    retrieveMovies
-} from "../../../../api/moviesApi.ts";
-import Movie from "../../../../types/movie.ts";
+import { homePageLoader, homePageQuery } from "../../../../loaders/homePageLoader";
 import Hero from "./Hero";
 import RatingList from "./RatingList";
 import RecentMoviesRow from "./RecentMoviesRow";
 
-const homePageQuery = () => ({
-    queryKey: ["homePage"],
-    queryFn: async () => {
-        const recentMovies = await retrieveMovies(getNowPlayingMoviesPath());
-        const lovedMovies = await retrieveMovies(getMostLovedMoviesPath());
-        const hatedMovies = await retrieveMovies(getMostHatedMoviesPath());
-        const classicMovies = await retrieveMovies(getClassicMoviesPath());
-        const upcomingMovies = await retrieveMovies(getUpcomingMoviesPath());
-        return { recentMovies, lovedMovies, hatedMovies, classicMovies, upcomingMovies };
-    }
-});
-
-const homePageLoader = (queryClient: QueryClient) => async () => {
-    return queryClient.getQueryData(homePageQuery().queryKey) ?? (await queryClient.fetchQuery(homePageQuery()));
-};
-
-interface LoaderData {
-    recentMovies: Movie[];
-    lovedMovies: Movie[];
-    hatedMovies: Movie[];
-    classicMovies: Movie[];
-    upcomingMovies: Movie[];
-}
-
 const HomePage: React.FC = () => {
     const initialData = useLoaderData() as Awaited<ReturnType<ReturnType<typeof homePageLoader>>>;
-    const { data } = useQuery({ ...homePageQuery(), initialData });
-    const { recentMovies, lovedMovies, hatedMovies, classicMovies, upcomingMovies } = data as LoaderData;
+    const {
+        data: { recentMovies, lovedMovies, hatedMovies, classicMovies, upcomingMovies }
+    } = useQuery({ ...homePageQuery, initialData });
 
     useEffect(() => {
         document.title = "CineSplain - The Movie Info App";
@@ -99,5 +68,4 @@ const HomePage: React.FC = () => {
     );
 };
 
-export { homePageLoader };
 export default HomePage;
