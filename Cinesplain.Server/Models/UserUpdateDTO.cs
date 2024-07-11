8b963bb3cb6ed2c77decb7d402f73fc1897432c8
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-11 04:39:27 -0700

    feat: add UserUpdateDTO model

---
+++ Cinesplain.Server/Models/UserUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Cinesplain.Server.Models;
+
+public class UserUpdateDTO
+{
+    public string? FirstName  { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+}
