dc56e10a4be1e740effca0a96717ca9aa8d6ad07
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-11 04:35:18 -0700

    feat: add CinesplainController

---
+++ Cinesplain.Server/Controllers/CinesplainController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cinesplain.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+public class CinesplainController : ControllerBase { };
