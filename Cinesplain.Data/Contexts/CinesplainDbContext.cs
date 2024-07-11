04d38cf73b80e45c0798d81a267abd43e9159b3f
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-11 04:25:09 -0700

    feat: add CinesplainDbContext for managing database operations

---
+++ Cinesplain.Data/Contexts/CinesplainDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Cinesplain.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinesplain.Data.Contexts;
+
+public class CinesplainDbContext(DbContextOptions<CinesplainDbContext> options) : IdentityDbContext<CinesplainUser>(options)
+{
+    public DbSet<Favorite> Favorites { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<CinesplainUser>().ToTable("Users");
+    }
+}
