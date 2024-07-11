0c63f0a09fd982a8ccb7395068ef00cdf6bc91b4
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-11 04:25:25 -0700

    feat: add CinesplainUser entity

---
+++ Cinesplain.Data/Entities/CinesplainUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinesplain.Data.Entities
+{
+    [Table("users")]
+    public class CinesplainUser() : IdentityUser
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        public List<Favorite> Favorites { get; set; } = [];
+    }
+}
