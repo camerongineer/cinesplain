d3dc12a51b26e6ac4d57a96fd2b54fe400846902
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-11 04:39:17 -0700

    feat: add FavoriteRetrievalDTO model

---
+++ Cinesplain.Server/Models/FavoriteRetrievalDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Cinesplain.Server.Models;
+
+public class FavoriteRetrievalDTO(long id, string contentId)
+{
+    public long FavoriteId { get; set; } = id;
+    public string ContentId { get; set; } = contentId;  
+}
