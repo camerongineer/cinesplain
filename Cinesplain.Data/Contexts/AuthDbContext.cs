9b56416c3e5a5326e4ffc1fdf47e573205a2b19a
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-11 04:24:54 -0700

    feat: add AuthDbContext to handle authentication-related database operations

---
+++ Cinesplain.Data/Contexts/AuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinesplain.Data.Contexts;
+
+public class AuthDbContext<T>(DbContextOptions<AuthDbContext<T>> options) : IdentityDbContext<T>(options) where T : IdentityUser
+{
+    override public DbSet<T> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
