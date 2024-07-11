a664c9050d27b04996e478f2d20c2bc9864be277
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-11 04:39:22 -0700

    feat: add UserRetrievalDTO model

---
+++ Cinesplain.Server/Models/UserRetrievalDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinesplain.Server.Models;
+
+public class UserRetrievalDTO
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string? Email { get; set; }
+    public List<FavoriteRetrievalDTO> Favorites { get; set; } = [];
+}
