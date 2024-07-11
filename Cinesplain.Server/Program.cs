0f8e108d19bad619cdceb121d4c15424af5c61bd
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-11 04:46:14 -0700

    feat: add authentication and authorization middleware

--- Cinesplain.Server/Program.cs
+++ Cinesplain.Server/Program.cs
@@ -1,13 +1,22 @@
+using Cinesplain.Data.Contexts;
+using Cinesplain.Data.Entities;
+using Cinesplain.Server.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddOutputCache();
+ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) {
+app.MapIdentityApi<CinesplainUser>();
+
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -16,6 +25,7 @@ app.UseFileServer();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -25,3 +35,62 @@ app.MapFallbackToFile("/index.html");
 app.UseOutputCache();
 
 app.Run();
+
+
+static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+{
+    var connection = configuration.GetConnectionString("DefaultConnection");
+
+    services.AddLogging(config =>
+    {
+        config.AddConsole();
+        config.AddDebug();
+    });
+
+    services.AddDbContext<CinesplainDbContext>(options => {
+        options.UseSqlServer(connection, b => b.MigrationsAssembly("Cinesplain.Server"));
+    });
+
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(configuration);
+
+    services.AddIdentityApiEndpoints<CinesplainUser>()
+        .AddEntityFrameworkStores<CinesplainDbContext>();
+
+    services.AddScoped<CinesplainUserManager>();
+
+    services.AddAuthorization();
+
+    services.AddControllers();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new()
+        {
+            Title = "Cinesplain API",
+            Version = "v1"
+        });
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "Please enter the API token",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT"
+        });
+
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            []
+        }
+    });
+    });
+    services.AddOutputCache();
+}
