4e08fba495246ac51449bfb08b1393ed63411862
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-11 04:25:37 -0700

    feat: add Favorite entity

---
+++ Cinesplain.Data/Entities/Favorite.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinesplain.Data.Entities;
+
+public class Favorite(string userId, string contentId)
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long FavoriteId { get; set; }
+
+    [Required]
+    public string UserId { get; set; } = userId;
+
+    public CinesplainUser? User { get; set; }
+
+    [Required]
+    [MaxLength(36)]
+    public string ContentId { get; set; } = contentId;
+}
