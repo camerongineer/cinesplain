4d9d498aab5acfa7d299c19924e5c529def3fdc4
Cameron Marr <alphaomegabeta@gmail.com>
2024-07-28 15:20:37 -0700

    feat: Add UserRegisterDTO model for user registration

---
+++ Cinesplain.Server/Models/UserRegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿using Cinesplain.Data.Entities;
+
+namespace Cinesplain.Server.Models;
+
+public partial class UserRegisterDTO
+{
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+}
