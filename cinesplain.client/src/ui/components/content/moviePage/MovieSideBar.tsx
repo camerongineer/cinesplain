ed7625bfc0b5ac3c1aa5b595d7c067c7cf3514f6
Cameron Marr <alphaomegabeta@gmail.com>
2024-09-14 07:00:02 -0700

    style: Format file

--- cinesplain.client/src/ui/components/content/moviePage/MovieSideBar.tsx
+++ cinesplain.client/src/ui/components/content/moviePage/MovieSideBar.tsx
@@ -28,60 +28,66 @@ interface MovieSideBarProps {
     omdbDetails?: omdbMovieDetails | null;
 }
 
-const MovieSideBar: React.FC<MovieSideBarProps> = ({ movie, omdbDetails }) => (
-    <StyledStack
-        flex={{
-            md: 1,
-            lg: 1
-        }}
-        justifyContent="center"
-        padding={1}
-        marginX={1}
-        order={{
-            xs: -1,
-            md: 0
-        }}
-    >
-        {movie.tagline && <TaglineDisplay tagline={movie.tagline} />}
-        {movie.overview && <SplainationDisplay overview={movie.overview} />}
-        <Stack alignItems="center" spacing={1}>
-            <Stack spacing={2} pb={2}>
-                {omdbDetails?.director && omdbDetails?.director !== "N/A" && (
-                    <DirectorDisplay director={omdbDetails.director} />
+const MovieSideBar: React.FC<MovieSideBarProps> = ({ movie, omdbDetails }) => {
+    return (
+        <StyledStack
+            flex={{
+                md: 1,
+                lg: 1,
+            }}
+            justifyContent="center"
+            padding={1}
+            marginX={1}
+            order={{
+                xs: -1,
+                md: 0,
+            }}
+        >
+            {movie.tagline && <TaglineDisplay tagline={movie.tagline} />}
+            {movie.overview && <SplainationDisplay overview={movie.overview} />}
+            <Stack alignItems="center" spacing={1}>
+                <Stack spacing={2} pb={2}>
+                    {omdbDetails?.director && omdbDetails?.director !== "N/A" && (
+                        <DirectorDisplay director={omdbDetails.director} />
+                    )}
+                    {omdbDetails?.writer && omdbDetails?.writer !== "N/A" && (
+                        <WritersDisplay writers={omdbDetails.writer} />
+                    )}
+                    {omdbDetails?.actors && omdbDetails?.actors !== "N/A" && (
+                        <StarringDisplay stars={omdbDetails.actors} />
+                    )}
+                    <RatingCard movie={movie} omdbDetails={omdbDetails} />
+                </Stack>
+                {!!movie.runtime && <RuntimeDisplay runtime={movie.runtime} includeLabel={true} />}
+                {!!movie.releaseDate && <ReleaseDateDisplay releaseDate={movie.releaseDate} includeLabel={true} />}
+                {!!movie.budget && <CurrencyDisplay labelText="Budget" currencyAmount={movie.budget} />}
+                {!!movie.revenue && <CurrencyDisplay labelText="Revenue" currencyAmount={movie.revenue} />}
+                {omdbDetails?.awards && omdbDetails?.awards !== "N/A" && (
+                    <AccoladesDisplay awards={omdbDetails.awards} />
                 )}
-                {omdbDetails?.writer && omdbDetails?.writer !== "N/A" && (
-                    <WritersDisplay writers={omdbDetails.writer} />
-                )}
-                {omdbDetails?.actors && omdbDetails?.actors !== "N/A" && <StarringDisplay stars={omdbDetails.actors} />}
-                <RatingCard movie={movie} omdbDetails={omdbDetails} />
+                <Stack flexDirection="row" justifyContent="space-evenly" alignItems="center" width="100%">
+                    {movie.homepage && (
+                        <RouterLink to={movie.homepage} target="_blank" rel="noopener noreferrer">
+                            <MuiLink component="span" variant="overline" fontSize="large" fontWeight="bolder">
+                                Webpage
+                            </MuiLink>
+                        </RouterLink>
+                    )}
+                    {movie.imdbId && (
+                        <Link to={getImdbMoviePath(movie.imdbId)} target="_blank" rel="noopener noreferrer">
+                            <Box
+                                component="img"
+                                height="25px"
+                                src={imdb as unknown as string}
+                                alt="Link to IMDB"
+                                mt={0.75}
+                            />
+                        </Link>
+                    )}
+                </Stack>
             </Stack>
-            {!!movie.runtime && <RuntimeDisplay runtime={movie.runtime} includeLabel={true} />}
-            {!!movie.releaseDate && <ReleaseDateDisplay releaseDate={movie.releaseDate} includeLabel={true} />}
-            {!!movie.budget && <CurrencyDisplay labelText="Budget" currencyAmount={movie.budget} />}
-            {!!movie.revenue && <CurrencyDisplay labelText="Revenue" currencyAmount={movie.revenue} />}
-            {omdbDetails?.awards && omdbDetails?.awards !== "N/A" && <AccoladesDisplay awards={omdbDetails.awards} />}
-            <Stack flexDirection="row" justifyContent="space-evenly" alignItems="center" width="100%">
-                {movie.homepage && (
-                    <RouterLink to={movie.homepage} target="_blank" rel="noopener noreferrer">
-                        <MuiLink component="span" variant="overline" fontSize="large" fontWeight="bolder">
-                            Webpage
-                        </MuiLink>
-                    </RouterLink>
-                )}
-                {movie.imdbId && (
-                    <Link to={getImdbMoviePath(movie.imdbId)} target="_blank" rel="noopener noreferrer">
-                        <Box
-                            component="img"
-                            height="25px"
-                            src={imdb as unknown as string}
-                            alt="Link to IMDB"
-                            mt={0.75}
-                        />
-                    </Link>
-                )}
-            </Stack>
-        </Stack>
-    </StyledStack>
-);
+        </StyledStack>
+    );
+};
 
 export default MovieSideBar;
