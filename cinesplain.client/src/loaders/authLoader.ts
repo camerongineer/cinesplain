2f38f42fa3d72b10b514b38e2336533932b0dd29
Cameron Marr <alphaomegabeta@gmail.com>
2024-09-14 06:53:26 -0700

    refactor: Update authLoader to use axios for API calls and handle error responses

--- cinesplain.client/src/loaders/authLoader.ts
+++ cinesplain.client/src/loaders/authLoader.ts
@@ -1,20 +1,14 @@
-import UserInfo from "../types/userInfo.ts";
+import axios, { isAxiosError } from "axios";
+import { redirect } from "react-router-dom";
 
-const authLoader = async (): Promise<UserInfo | null> => {
+const authLoader = async () => {
     try {
-        const response = await fetch("/.auth/me");
-        const payload = await response.json();
-        const { clientPrincipal } = payload;
-        if (clientPrincipal.identityProvider === "aad") {
-            clientPrincipal.identityDetails = "Microsoft";
-        } else if (clientPrincipal.identityProvider === "github") {
-            clientPrincipal.identityDetails = "Github";
-        } else {
-            clientPrincipal.identityDetails = null;
-        }
-        return clientPrincipal;
-    } catch (error) {
+        const response = await axios.get("/api/user");
+        if (response.status === 200) redirect("/");
         return null;
+    } catch (error) {
+        if (isAxiosError(error) && error.response?.status === 401) return redirect("/login");
+        throw error;
     }
 };
 
