7bf1ce4de54a5fd45e43a8e1cc91e623656f8f8b
Cameron M. <93474097+camerongineer@users.noreply.github.com>
2025-05-31 08:09:47 -0700

    test: Add unit tests for crew credits in Cinesplain.Server.Test

---
+++ Cinesplain.Server.Test/CinesplainTests.cs
@@ -0,0 +1,93 @@
+using Cinesplain.Server.Utilities;
+using TMDBModels.Models;
+
+namespace Cinesplain.Server.Test;
+
+public class CinesplainTests
+{
+    [Fact]
+    public void CombineCrewCredits_ShouldCombineCrewCreditsWithSameId()
+    {
+        // Arrange
+        var crewCredits = new List<ListDisplayMovieCrewCredit>
+        {
+            new()
+            {
+                Id = 1,
+                Job = "Director",
+                Department = "Production"
+            },
+            new()
+            {
+                Id = 2,
+                Job = "Writer",
+                Department = "Writing"
+            },
+            new()
+            {
+                Id = 1,
+                Job = "Producer",
+                Department = "Production"
+            },
+            new()
+            {
+                Id = 3,
+                Job = "Editor",
+                Department = "Editing"
+            },
+            new()
+            {
+                Id = 4,
+                Job = "Cinematographer",
+                Department = "Cinematography"
+            },
+            new()
+            {
+                Id = 1,
+                Job = "Screenwriter",
+                Department = "Writing"
+            }
+        };
+
+        var expectedCredits = new List<ListDisplayMovieCrewCredit>
+        {
+            new()
+            {
+                Id = 1,
+                Job = "Director, Producer, Screenwriter",
+                Department = "Production, Writing"
+            },
+            new()
+            {
+                Id = 2,
+                Job = "Writer",
+                Department = "Writing"
+            },
+            new()
+            {
+                Id = 3,
+                Job = "Editor",
+                Department = "Editing"
+            },
+            new()
+            {
+                Id = 4,
+                Job = "Cinematographer",
+                Department = "Cinematography"
+            }
+        };
+
+        // Act
+        var combinedCredits = ApiUtility.CombineCrewCredits(crewCredits);
+
+        // Assert
+        foreach (var expectedCredit in expectedCredits)
+        {
+            var combinedCredit = combinedCredits.FirstOrDefault(c => c.Id == expectedCredit.Id);
+            Assert.NotNull(combinedCredit);
+
+            Assert.Equal(expectedCredit.Job, combinedCredit.Job);
+            Assert.Equal(expectedCredit.Department, combinedCredit.Department);
+        }
+    }
+}
