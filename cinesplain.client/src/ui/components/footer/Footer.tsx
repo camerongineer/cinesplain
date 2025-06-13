67b5825c9c6a856bc5168adbc5aa500eb8825994
Cameron M <93474097+camerongineer@users.noreply.github.com>
2025-06-12 21:11:14 -0700

    feat: Implement loading screen visibility with delay in Footer component

--- cinesplain.client/src/ui/components/footer/Footer.tsx
+++ cinesplain.client/src/ui/components/footer/Footer.tsx
@@ -1,11 +1,13 @@
 import CopyrightDisplay from "@camerongineer/reuse/components/CopywriteDisplay.tsx";
 import { Stack, styled } from "@mui/material";
+import { useQueryClient } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
 import DataCreditDisplay from "./DataCreditDisplay.tsx";
 import FooterLogoDisplay from "./FooterLogoDisplay.tsx";
 import LibraryIconDisplay from "./LibraryIconDisplay.tsx";
 import SourceCodeDisplay from "./SourceCodeDisplay.tsx";
 
-const StyledContainer = styled(Stack)`
+const StyledContainer = styled(Stack)<{ 'data-visible'?: boolean }>`
     justify-content: center;
     align-content: center;
     min-height: 100px;
@@ -19,6 +21,8 @@ const StyledContainer = styled(Stack)`
         ${(props) => props.theme.palette.background.paper},
         ${(props) => props.theme.palette.background.paper}
     );
+    opacity: ${(props) => (props['data-visible'] ? 1 : 0)};
+    transition: opacity 0.8s ease;
 `;
 
 const StyledContentWrapper = styled(Stack)`
@@ -30,18 +34,36 @@ const StyledContentWrapper = styled(Stack)`
     gap: 1em;
 `;
 
-const Footer: React.FC = () => (
-    <StyledContainer className="center">
-        <StyledContentWrapper direction={{ md: "row" }}>
-            <SourceCodeDisplay />
-            <Stack justifyContent="space-evenly" spacing={0.5} margin=".5em 1em" order={{ xs: 1, md: 0 }}>
-                <FooterLogoDisplay />
-                <CopyrightDisplay startYear={2023} endYear={2025} onLogoClick={() => window.open("https://www.linkedin.com/in/c-em/", "_blank")} />
-                <LibraryIconDisplay />
-            </Stack>
-            <DataCreditDisplay />
-        </StyledContentWrapper>
-    </StyledContainer>
-);
+
+const Footer: React.FC = () => {
+    const [visible, setVisible] = useState(false);
+    const queryClient = useQueryClient();
+    
+    useEffect(() => {
+        let timeoutId: NodeJS.Timeout;
+        if (!visible && !queryClient.isFetching()) {
+            timeoutId = setTimeout(() => {
+                setVisible(true);
+            }, 1000);
+        }
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [visible, queryClient]);
+
+    return (
+        <StyledContainer className="center" data-visible={visible}>
+            <StyledContentWrapper direction={{ md: "row" }}>
+                <SourceCodeDisplay />
+                <Stack justifyContent="space-evenly" spacing={0.5} margin=".5em 1em" order={{ xs: 1, md: 0 }}>
+                    <FooterLogoDisplay />
+                    <CopyrightDisplay startYear={2023} endYear={2025} onLogoClick={() => window.open("https://www.linkedin.com/in/c-em/", "_blank")} />
+                    <LibraryIconDisplay />
+                </Stack>
+                <DataCreditDisplay />
+            </StyledContentWrapper>
+        </StyledContainer>
+    );
+};
 
 export default Footer;
